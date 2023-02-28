da9cff9c18c18ef1bb8e51eca7938bf1ac6ff7db
Nzik2 <111058110+Nzik2@users.noreply.github.com>
2023-02-28 16:07:17 +0200

    Add files via upload

---
+++ hw8/BucketService.java
@@ -0,0 +1,5 @@
+package nazar.dao.service;
+import nazar.domain.Bucket;
+import nazar.shared.AbstractCRUD;
+public interface BucketService extends AbstractCRUD<Bucket> {
+}
---
+++ hw8/UserDao.java
@@ -0,0 +1,6 @@
+package nazar.dao;
+import nazar.domain.User;
+import nazar.shared.AbstractCRUD;
+public interface UserDao extends AbstractCRUD<User>{
+
+}
